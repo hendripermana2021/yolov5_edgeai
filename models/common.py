b24020af234249adfc48adfd9642ac33a39d8077
hendripermana2021 <hendripermana60@gmail.com>
2023-11-24 19:55:53 +0700

    uploud pt

--- models/common.py
+++ models/common.py
@@ -36,10 +36,10 @@ def DWConv(c1, c2, k=1, s=1, act=True):
 class Conv(nn.Module):
     # Standard convolution
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super().__init__()
+        super(Conv, self).__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.ReLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+        self.act = nn.ReLU(inplace=False) if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
